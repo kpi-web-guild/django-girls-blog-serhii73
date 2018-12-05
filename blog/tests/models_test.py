0c4336c221ff830d96cf6816385c1df71425f1af
serhii <aserhii@protonmail.com>
2018-12-05 14:45:08 +0200

    Fix errors

--- blog/tests/models_test.py
+++ blog/tests/models_test.py
@@ -16,28 +16,31 @@ class PostModelTest(TestCase):
     def setUp(self):
         """Set up non-modifiable objects used by all test methods."""
         self.post = Post.objects.create(
-            author=User.objects.create(username='bob'),
-            title='Title',
-            text='A few lines of text',
+            author=User.objects.create(username="bob"),
+            title="Title",
+            text="A few lines of text",
         )
 
     def tearDown(self):
         """Clean-up test data."""
         del self.post
 
-    def test_right_title(self):
-        """Test right title output."""
-        assert str(self.post) == 'Title'
+    def test_post_str(self):
+        """Test post stringification."""
+        assert str(self.post) == "Title"
 
     @patch(
-        'django.utils.timezone.now',
+        "django.utils.timezone.now",
         lambda: datetime(
             day=11, month=9, year=2018, tzinfo=timezone.get_current_timezone()
         ),
     )
     def test_post_publish(self):
-        """Test for publish method."""
+        """Test post has a published date after publish method called."""
+        assert self.post.published_date is None
+
         self.post.publish()
+
         assert self.post.published_date == datetime(
             day=11, month=9, year=2018, tzinfo=timezone.get_current_timezone()
         )
